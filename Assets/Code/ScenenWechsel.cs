3e0e884a8f877dba9da6234481fed6b30e8e1948
Tughan <96036066+TughanOzer@users.noreply.github.com>
2023-04-30 15:22:57 +0200

    Upd: Background and effects

---
+++ Assets/Code/ScenenWechsel.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenenWechsel : MonoBehaviour
+{
+    public GameObject Scene1;
+    public GameObject Scene2;
+    public GameObject Scene3;
+
+    public bool destination1;
+    public bool destination2;
+    public bool destination3;
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(other);
+        if (other.CompareTag("Player")){
+            if (destination1) {
+                Scene1.SetActive(true);
+                Scene2.SetActive(false);
+                Scene3.SetActive(false);
+            }
+            else if (destination2) {
+                Scene1.SetActive(false);
+                Scene2.SetActive(true);
+                Scene3.SetActive(false);
+            }
+            else if (destination3) {
+                Scene1.SetActive(false);
+                Scene2.SetActive(false);
+                Scene3.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Extern/UI Asset Store/Realistic Glitches Lite/Assets/Scripts/ShaderEffect_CRT.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShaderEffect_CRT : MonoBehaviour {
+
+	public float scanlineIntensity = 100;
+	public int scanlineWidth = 1;
+//	public Color scanlineColor = Color.black;
+//	public bool tVBulge = true;
+	private Material material_Displacement;
+	public Material material_Scanlines;
+
+	private float ScrollX = 0.5f;
+	private float ScrollY = 0.5f;
+	float position = 0;
+    public float speed = 1;
+
+    void Awake ()
+	{
+		material_Scanlines = new Material( Shader.Find("Hidden/Scanlines") );
+	}
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination)
+	{
+		material_Scanlines.SetFloat("_Intensity", scanlineIntensity * 0.01f);
+		material_Scanlines.SetFloat("_ValueX", scanlineWidth);
+
+		Graphics.Blit (source, destination, material_Scanlines);
+        //      float OffsetY = Time.time * ScrollY;
+        //      float OffsetX = Time.time * ScrollY;
+        //Debug.Log(OffsetX);
+        //      material_Scanlines.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+    }
+
+
+}
